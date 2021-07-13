ba1db1b599cce180d1156c1fbdd1e687cdbe9cc0
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-13 13:49:54 -0600

    cadds up and down functions inside the data/migrations/01-make_cars_table.js

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,22 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema
+    .createTable('cars', table => {
+      table.increments('id')
+      table.text('vin')
+        .unique()
+        .notNullable()
+      table.text('make')
+        .unique()
+      table.text('model')
+        .unique()
+      table.decimal('mileage')
+        .unique()
+      table.text('title')
+      table.text('transmission')
+    })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema
+    .dropTableIfExists('cars')
 };
